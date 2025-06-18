0c2016b5c4b56d0a1a293b4cf48d3766aa2b36f0
pranwsh <praneshk119@gmail.com>
2025-06-17 22:12:52 -0700

    fixed immutable lockfile

--- home/nvim/nvim/lua/config/options.lua
+++ home/nvim/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+require("lazy").setup({
+  -- your plugins here
+}, {
+  lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json",
+})
