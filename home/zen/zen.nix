970dd1be42a581e664083aad07e53286f29d2c4e
pranwsh <praneshk119@gmail.com>
2025-06-17 21:08:18 -0700

    zen fix

--- home/zen/zen.nix
+++ home/zen/zen.nix
@@ -1,40 +1,10 @@
-# { pkgs, ... }:
-# let
-#   zenBrowserSrc = pkgs.fetchFromGitHub {
-#     owner = "0xc000022070";
-#     repo = "zen-browser-flake";
-#     rev = "main";
-#     sha256 = "EgYaWbzbtYFLZH1/Eni1ZdLgTwYhe8g9m10UPdBMCmY=";
-#   };
-#
-#   # Use callPackage to get the package set, then select the default package
-#   zenBrowserPkgs = pkgs.callPackage zenBrowserSrc { };
-#   zenBrowser = zenBrowserPkgs.default;
-# in
-# {
-#   home.packages = [ zenBrowser ];
-# }
-#
-#
-# { config, pkgs, ... }:
-# let
-#   profileName = "ro4rmu9s.Default Profile";
-# in {
-#   home.file.".zen/${profileName}/chrome" = {
-#     source = ./config/chrome;
-#     recursive = true;
-#   };
-#   home.file.".zen/${profileName}/user.js" = {
-#     source = ./config/user.js;
-#   };
-# }
-#
 { config, pkgs, inputs, ... }:
 let
   profileName = "ro4rmu9s.Default Profile";
-in {
+in
+{
   # Install Zen browser package
-  home.packages = with pkgs; [
+  home.packages = [
     inputs.zen-browser.packages.${pkgs.system}.default
   ];
 
@@ -47,3 +17,4 @@ in {
   home.file.".zen/${profileName}/user.js" = {
     source = ./config/user.js;
   };
+}
