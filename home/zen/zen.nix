9fbc1bd1b01afa076aee6e9451c8794e51647806
pranwsh <praneshk119@gmail.com>
2025-06-17 20:38:29 -0700

    zen fix

--- home/zen/zen.nix
+++ home/zen/zen.nix
@@ -16,15 +16,34 @@
 # }
 #
 #
-{ config, pkgs, ... }:
+# { config, pkgs, ... }:
+# let
+#   profileName = "ro4rmu9s.Default Profile";
+# in {
+#   home.file.".zen/${profileName}/chrome" = {
+#     source = ./config/chrome;
+#     recursive = true;
+#   };
+#   home.file.".zen/${profileName}/user.js" = {
+#     source = ./config/user.js;
+#   };
+# }
+#
+{ config, pkgs, inputs, ... }:
 let
   profileName = "ro4rmu9s.Default Profile";
 in {
+  # Install Zen browser package
+  home.packages = with pkgs; [
+    inputs.zen-browser.packages.${pkgs.system}.default
+  ];
+
+  # Configure Zen browser profile with custom chrome and user.js
   home.file.".zen/${profileName}/chrome" = {
     source = ./config/chrome;
     recursive = true;
   };
+  
   home.file.".zen/${profileName}/user.js" = {
     source = ./config/user.js;
   };
-}
