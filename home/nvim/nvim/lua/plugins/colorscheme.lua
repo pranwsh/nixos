24e0de1e99bd282bb7390d7ed851d3a6ae0ab74b
pranwsh <praneshk119@gmail.com>
2025-06-17 21:23:09 -0700

    nvim transparency

--- home/nvim/nvim/lua/plugins/colorscheme.lua
+++ home/nvim/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,68 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
     },
+    on_colors = function(colors)
+      -- Make backgrounds fully transparent
+      colors.bg = "NONE"
+      colors.bg_dark = "NONE"
+      colors.bg_float = "NONE"
+      colors.bg_popup = "NONE"
+      colors.bg_sidebar = "NONE"
+      colors.bg_statusline = "NONE"
+    end,
+    on_highlights = function(highlights, colors)
+      -- Additional transparency overrides
+      highlights.Normal = { bg = "NONE" }
+      highlights.NormalFloat = { bg = "NONE" }
+      highlights.FloatBorder = { bg = "NONE" }
+      highlights.Pmenu = { bg = "NONE" }
+      highlights.PmenuSel = { bg = colors.bg_highlight }
+      highlights.PmenuSbar = { bg = "NONE" }
+      highlights.PmenuThumb = { bg = colors.fg_gutter }
+      highlights.StatusLine = { bg = "NONE" }
+      highlights.StatusLineNC = { bg = "NONE" }
+      highlights.TabLine = { bg = "NONE" }
+      highlights.TabLineFill = { bg = "NONE" }
+      highlights.TabLineSel = { bg = "NONE" }
+      highlights.WinBar = { bg = "NONE" }
+      highlights.WinBarNC = { bg = "NONE" }
+      highlights.SignColumn = { bg = "NONE" }
+      highlights.FoldColumn = { bg = "NONE" }
+      highlights.LineNr = { bg = "NONE" }
+      highlights.CursorLineNr = { bg = "NONE" }
+      highlights.EndOfBuffer = { bg = "NONE" }
+      
+      -- Telescope transparency
+      highlights.TelescopeNormal = { bg = "NONE" }
+      highlights.TelescopeBorder = { bg = "NONE" }
+      highlights.TelescopePromptNormal = { bg = "NONE" }
+      highlights.TelescopePromptBorder = { bg = "NONE" }
+      highlights.TelescopeResultsNormal = { bg = "NONE" }
+      highlights.TelescopeResultsBorder = { bg = "NONE" }
+      highlights.TelescopePreviewNormal = { bg = "NONE" }
+      highlights.TelescopePreviewBorder = { bg = "NONE" }
+      
+      -- Neo-tree transparency
+      highlights.NeoTreeNormal = { bg = "NONE" }
+      highlights.NeoTreeNormalNC = { bg = "NONE" }
+      highlights.NeoTreeEndOfBuffer = { bg = "NONE" }
+      
+      -- Which-key transparency
+      highlights.WhichKeyFloat = { bg = "NONE" }
+      
+      -- Notify transparency
+      highlights.NotifyBackground = { bg = "NONE" }
+      
+      -- Mason transparency
+      highlights.MasonNormal = { bg = "NONE" }
+      
+      -- Lazy transparency
+      highlights.LazyNormal = { bg = "NONE" }
+    end,
   },
 }
