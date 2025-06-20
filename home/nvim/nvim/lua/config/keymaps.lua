5bdfc328cd0332158b3b209a622aa0042912ff5f
pranwsh <praneshk119@gmail.com>
2025-06-20 16:28:55 -0700

    compile bind

--- home/nvim/nvim/lua/config/keymaps.lua
+++ home/nvim/nvim/lua/config/keymaps.lua
@@ -2,14 +2,26 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-return {
-  {
-    "my-c-compile-run",
-    config = function()
-      vim.keymap.set('n', '<leader>r', function()
-        vim.cmd('write')
-        vim.cmd('!gcc % -o %:r && ./%:r')
-      end, { desc = 'Compile & run C file' })
-    end
-  }
-}
+
+
+-- Compile C
+vim.keymap.set("n", "<leader>rc", function()
+  local file = vim.fn.expand("%")
+  local file_no_ext = vim.fn.expand("%:r")
+  vim.cmd("w") -- Save file first
+  vim.cmd(string.format("!gcc -o %s %s", file_no_ext, file))
+end, vim.tbl_extend("force", opts, { desc = "Compile C file" }))
+
+-- Run compiled C
+vim.keymap.set("n", "<leader>rw", function()
+  local file_no_ext = vim.fn.expand("%:r")
+  vim.cmd("!" .. file_no_ext)
+end, vim.tbl_extend("force", opts, { desc = "Run C program" }))
+
+-- Compile and run C
+vim.keymap.set("n", "<leader>rr", function()
+  local file = vim.fn.expand("%")
+  local file_no_ext = vim.fn.expand("%:r")
+  vim.cmd("w") -- Save file first
+  vim.cmd(string.format("!gcc -o %s %s && %s", file_no_ext, file, file_no_ext))
+end, vim.tbl_extend("force", opts, { desc = "Compile and run C file" }))
