e7a57bc5fc105760bc475237a0de12e70bc744f7
pranwsh <praneshk119@gmail.com>
2025-06-20 03:19:24 -0700

    style

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -219,6 +219,7 @@
         "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
         # "opacity 0.42,class:^(org\.pwmt\.zathura)$"
+        "opacity 0.4 0.4, class:^(spotify)$"
       ];
     };
   };
--- home/spotify/spotify.nix
+++ home/spotify/spotify.nix
@@ -2,25 +2,41 @@
 {
   imports = [ inputs.spicetify-nix.homeManagerModules.default ];
   programs.spicetify =
-  let
-    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-  in
-  {
-    enable = true;
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in
+      {
+      enable = true;
 
-    enabledExtensions = with spicePkgs.extensions; [
-      adblock
-      hidePodcasts
-      shuffle # shuffle+ (special characters are sanitized out of extension names)
-    ];
-    enabledCustomApps = with spicePkgs.apps; [
-      marketplace
-    ];
-    enabledSnippets = with spicePkgs.snippets; [
-    ];
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        hidePodcasts
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
+      ];
+      enabledCustomApps = with spicePkgs.apps; [
+      ];
+      enabledSnippets = with spicePkgs.snippets; [
+      ];
 
-    theme = spicePkgs.themes.catppuccin;
-    colorScheme = "mocha";
-  };
+      colorScheme = "custom";
+      customColorScheme = {
+        text = "ffffff";
+        subtext = "b3b3b3";
+        sidebar-text = "ffffff";
+        main = "00000066";
+        sidebar = "00000066";
+        player = "00000066";
+        card = "00000066";
+        shadow = "00000066";
+        selected-row = "00000066";
+        button = "00000066";
+        button-active = "00000066";
+        button-disabled = "00000066";
+        tab-active = "00000066";
+        notification = "00000066";
+        notification-error = "00000066";
+        misc = "00000066";
+      };
+    };
 }
 
