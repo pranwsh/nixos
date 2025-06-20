a2bc7c98f3b3a553d6130b99cfb7db6c126fbc54
pranwsh <praneshk119@gmail.com>
2025-06-19 17:36:27 -0700

    stylix

--- home/stylix/stylix.nix
+++ home/stylix/stylix.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }: {
+{ inputs, pkgs, lib, ... }: {
   imports = [
     inputs.stylix.homeModules.stylix
   ];
@@ -8,9 +8,29 @@
 
     # Base16 color scheme - you can change this to any base16 scheme
     # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-    
+
     # Or use an image to generate colors automatically
-    image = ./wallpapers/macdull;
+    # image = ./wallpapers/macblue;
+
+    base16Scheme = {
+      base00 = "000000";  # Default Background
+      base01 = "1a1a1a";  # Lighter Background (status bars)
+      base02 = "2d2d2d";  # Selection Background
+      base03 = "404040";  # Comments, Invisibles
+      base04 = "666666";  # Dark Foreground (status bars)
+      base05 = "cccccc";  # Default Foreground
+      base06 = "e0e0e0";  # Light Foreground (not often used)
+      base07 = "ffffff";  # Light Background (not often used)
+      base08 = "e84f85";  # Red/Pink
+      base09 = "e8784f";  # Orange
+      base0A = "f4d03f";  # Yellow
+      base0B = "58d68d";  # Green
+      base0C = "5dade2";  # Cyan
+      base0D = "5d8cff";  # Blue
+      base0E = "af7dff";  # Magenta/Purple
+      base0F = "8b4513";  # Brown
+    };
+
 
     # Font configuration
     # fonts = {
