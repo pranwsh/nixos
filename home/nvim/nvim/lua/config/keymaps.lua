7062f4d884fe474de91b47053f374aceeab9d74a
pranwsh <praneshk119@gmail.com>
2025-06-20 16:31:59 -0700

    compile bind

--- home/nvim/nvim/lua/config/keymaps.lua
+++ home/nvim/nvim/lua/config/keymaps.lua
@@ -2,26 +2,38 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- C file compile and run keymaps
+vim.keymap.set("n", "<leader>cc", function()
+  local file = vim.fn.expand("%:p")
+  local file_no_ext = vim.fn.expand("%:p:r")
+  local cmd = string.format("gcc -o %s %s", file_no_ext, file)
+  vim.cmd("!" .. cmd)
+end, { desc = "Compile C file" })
 
-
--- Compile C
-vim.keymap.set("n", "<leader>rc", function()
-  local file = vim.fn.expand("%")
-  local file_no_ext = vim.fn.expand("%:r")
-  vim.cmd("w") -- Save file first
-  vim.cmd(string.format("!gcc -o %s %s", file_no_ext, file))
-end, vim.tbl_extend("force", opts, { desc = "Compile C file" }))
-
--- Run compiled C
-vim.keymap.set("n", "<leader>rw", function()
-  local file_no_ext = vim.fn.expand("%:r")
+vim.keymap.set("n", "<leader>cr", function()
+  local file_no_ext = vim.fn.expand("%:p:r")
   vim.cmd("!" .. file_no_ext)
-end, vim.tbl_extend("force", opts, { desc = "Run C program" }))
+end, { desc = "Run compiled C program" })
 
--- Compile and run C
-vim.keymap.set("n", "<leader>rr", function()
-  local file = vim.fn.expand("%")
-  local file_no_ext = vim.fn.expand("%:r")
-  vim.cmd("w") -- Save file first
-  vim.cmd(string.format("!gcc -o %s %s && %s", file_no_ext, file, file_no_ext))
-end, vim.tbl_extend("force", opts, { desc = "Compile and run C file" }))
+vim.keymap.set("n", "<leader>cb", function()
+  local file = vim.fn.expand("%:p")
+  local file_no_ext = vim.fn.expand("%:p:r")
+  local compile_cmd = string.format("gcc -o %s %s", file_no_ext, file)
+  vim.cmd("!" .. compile_cmd .. " && " .. file_no_ext)
+end, { desc = "Compile and run C file" })
+
+-- Alternative using terminal
+vim.keymap.set("n", "<leader>ct", function()
+  local file = vim.fn.expand("%:p")
+  local file_no_ext = vim.fn.expand("%:p:r") 
+  local cmd = string.format("gcc -o %s %s && %s", file_no_ext, file, file_no_ext)
+  vim.cmd("terminal " .. cmd)
+end, { desc = "Compile and run C file in terminal" })
+
+-- With debug flags
+vim.keymap.set("n", "<leader>cd", function()
+  local file = vim.fn.expand("%:p")
+  local file_no_ext = vim.fn.expand("%:p:r")
+  local cmd = string.format("gcc -g -Wall -o %s %s && %s", file_no_ext, file, file_no_ext)
+  vim.cmd("!" .. cmd)
+end, { desc = "Compile C file with debug flags and run" })
