1084dc87ff5fdb0f6c5fac87b38e24c6799d392b
pranwsh <praneshk119@gmail.com>
2025-06-20 17:11:05 -0700

    keymap

--- home/nvim/nvim/lua/config/keymaps.lua
+++ home/nvim/nvim/lua/config/keymaps.lua
@@ -2,38 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- C file compile and run keymaps
-vim.keymap.set("n", "<leader>cc", function()
-  local file = vim.fn.expand("%:p")
-  local file_no_ext = vim.fn.expand("%:p:r")
-  local cmd = string.format("gcc -o %s %s", file_no_ext, file)
-  vim.cmd("!" .. cmd)
-end, { desc = "Compile C file" })
+local map = vim.keymap.set
 
-vim.keymap.set("n", "<leader>cr", function()
-  local file_no_ext = vim.fn.expand("%:p:r")
-  vim.cmd("!" .. file_no_ext)
-end, { desc = "Run compiled C program" })
+-- Compile & run C from current buffer
+map("n", "<leader>rr",
+  ":w<Bar>:TermExec cmd='gcc -std=c18 \"%\" -o \"%:r\" && ./%:r'<CR>",
+  { desc = "Build & Run C" }
+)
 
-vim.keymap.set("n", "<leader>cb", function()
-  local file = vim.fn.expand("%:p")
-  local file_no_ext = vim.fn.expand("%:p:r")
-  local compile_cmd = string.format("gcc -o %s %s", file_no_ext, file)
-  vim.cmd("!" .. compile_cmd .. " && " .. file_no_ext)
-end, { desc = "Compile and run C file" })
-
--- Alternative using terminal
-vim.keymap.set("n", "<leader>ct", function()
-  local file = vim.fn.expand("%:p")
-  local file_no_ext = vim.fn.expand("%:p:r") 
-  local cmd = string.format("gcc -o %s %s && %s", file_no_ext, file, file_no_ext)
-  vim.cmd("terminal " .. cmd)
-end, { desc = "Compile and run C file in terminal" })
-
--- With debug flags
-vim.keymap.set("n", "<leader>cd", function()
-  local file = vim.fn.expand("%:p")
-  local file_no_ext = vim.fn.expand("%:p:r")
-  local cmd = string.format("gcc -g -Wall -o %s %s && %s", file_no_ext, file, file_no_ext)
-  vim.cmd("!" .. cmd)
-end, { desc = "Compile C file with debug flags and run" })
+-- Compile with debug symbols and launch gdb
+map("n", "<leader>rd",
+  ":w<Bar>:TermExec cmd='gcc -g -std=c18 \"%\" -o \"%:r\" && gdb ./%:r'<CR>",
+  { desc = "Build & Debug C" }
+)
