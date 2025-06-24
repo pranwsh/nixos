795377af0fd80228143a6fde93e21988535e2b30
pranwsh <praneshk119@gmail.com>
2025-06-23 17:02:19 -0700

    toggleterm

--- home/nvim/nvim/lua/plugins/toggleterm.lua
+++ home/nvim/nvim/lua/plugins/toggleterm.lua
@@ -1,5 +1,26 @@
 return {
   {
-    'akinsho/toggleterm.nvim', version = "*", config = true
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        shade_terminals = false,
+        highlights = {
+          Normal = {
+            guibg = "NONE",
+          },
+          NormalFloat = {
+            link = 'Normal'
+          },
+          FloatBorder = {
+            guifg = "NONE",
+            guibg = "NONE",
+          },
+        },
+        float_opts = {
+          border = 'none',
+        },
+      })
+    end,
   },
 }
