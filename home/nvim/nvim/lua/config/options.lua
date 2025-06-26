0d90e94976e007eeee1d0906fa564bbeea5dad1a
pranwsh <praneshk119@gmail.com>
2025-06-26 14:12:53 -0700

    remove virtualtext highlight group

--- home/nvim/nvim/lua/config/options.lua
+++ home/nvim/nvim/lua/config/options.lua
@@ -4,3 +4,8 @@
 
 vim.opt.cursorline = false
 vim.o.laststatus = 0
+
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { bg = "NONE" })
