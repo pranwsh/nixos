9e220f9153d1ae196f6fe2482d5b32a4d86d8353
pranwsh <praneshk119@gmail.com>
2025-10-09 13:01:39 -0700

    rebuild

--- modules/virtualbox.nix
+++ modules/virtualbox.nix
@@ -1,7 +1,10 @@
-
 { config, pkgs, ... }:
-
 {
-   virtualisation.virtualbox.host.enable = true;
-   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+  
+  # Add VirtualBox to system packages
+  environment.systemPackages = with pkgs; [
+    virtualbox
+  ];
 }
