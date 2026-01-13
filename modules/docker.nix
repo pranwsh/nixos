a158dda1e4c6b34b701ccc78c4befebb76775764
pranwsh <praneshk119@gmail.com>
2026-01-12 17:25:11 -0800

    refactor

--- modules/docker.nix
+++ modules/docker.nix
@@ -5,6 +5,5 @@
     enable = true;
   };
 
-# Optional: Add your user to the "docker" group to run docker without sudo
   users.users.pranesh.extraGroups = [ "docker" ];
 }
--- modules/kvm.nix
+++ modules/kvm.nix
@@ -1,11 +1,6 @@
 { config, pkgs, ... }:
 {
-  # Enable KVM virtualization
   virtualisation.libvirtd.enable = true;
-  
-  # Add your user to the libvirtd and kvm groups
   users.users.pranesh.extraGroups = [ "libvirtd" "kvm" ];
-  
-  # Optional but recommended: enable dnsmasq for VM networking
   virtualisation.libvirtd.qemu.package = pkgs.qemu_kvm;
 }
