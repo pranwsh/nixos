1e3af28f58e8de861f1d2a7f7d5dc26c747b39a1
pranwsh <praneshk119@gmail.com>
2026-01-21 10:51:20 -0800

    syntax fix

--- home/nvim/options/keymaps.nix
+++ home/nvim/options/keymaps.nix
@@ -1,12 +1,7 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
+{...}: {
   programs.nvf.settings.vim.maps = {
     normal = {
-      "leader<e>" = {
+      "<leader>e" = {
         action = "<cmd>Neotree toggle<CR>";
         silent = true;
         desc = "Toggle Neotree";
