1813ccae5e7a094b3f89733bccf7733c6c58202f
pranwsh <praneshk119@gmail.com>
2026-01-21 11:02:04 -0800

    filetree width

--- home/nvim/plugins/neotree.nix
+++ home/nvim/plugins/neotree.nix
@@ -3,6 +3,11 @@
     vim.filetree = {
       neo-tree = {
         enable = true;
+
+        setupOpts = {
+          add_blank_line_at_top = true;
+          window.width = 20;
+        };
       };
     };
   };
