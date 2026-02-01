bb8d187f1b24e8ddb008a937e1d20222be6df360
pranwsh <praneshk119@gmail.com>
2026-02-01 15:14:36 -0800

    nvim tabs config

--- home/nvim/plugins/bufferline.nix
+++ home/nvim/plugins/bufferline.nix
@@ -4,9 +4,18 @@
       tabline.nvimBufferline = {
         enable = true;
 
+        mappings = {
+          cycleNext = "L";
+          cyclePrevious = "H";
+        };
+
         setupOpts.options = {
-          seperator_style = "slope";
+          seperator_style = [" " " "];
           show_buffer_close_icons = false;
+          show_close_icons = false;
+          show_tab_indicators = false;
+          always_show_bufferline = false;
+          indicator.style = "none";
         };
       };
     };
