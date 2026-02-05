8223202c8fdae6ef61f9c4d5444c1cc0a74d356f
pranwsh <praneshk119@gmail.com>
2026-02-05 13:20:15 -0800

    make asm-lsp.toml

--- home/code/asm.nix
+++ home/code/asm.nix
@@ -2,11 +2,4 @@
   home.packages = with pkgs; [
     nasm
   ];
-  # This creates the physical file asm-lsp is looking for
-  home.file.".asm-lsp.toml".text = ''
-    assembler = "gas"
-    architecture = "x86"
-    mode = "32"
-    diagnostics = true
-  '';
 }
