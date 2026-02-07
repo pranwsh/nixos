58da5e68a584df37d3136c3d3bbcfe454ca9ec44
pranwsh <praneshk119@gmail.com>
2026-02-07 12:55:48 -0800

    asm indent

--- home/nvim/plugins/lsp/asm.nix
+++ home/nvim/plugins/lsp/asm.nix
@@ -1,27 +1,10 @@
 {...}: {
-  imports = [
-    ./c.nix
-    ./asm.nix
-    ./nix.nix
-  ];
   programs.nvf.settings = {
     vim = {
-      lsp = {
+      languages.assembly = {
         enable = true;
-        formatOnSave = true;
-      };
-      languages = {
-        enableTreesitter = true;
-        enableFormat = true;
-      };
-      # Explicitly enable Treesitter indent module
-      treesitter = {
-        fold = false;
-        enable = true;
-        indent.enable = true; # This is the key line
-      };
-      visuals = {
-        nvim-web-devicons.enable = true;
+        treesitter.enable = true;
+        lsp.enable = true;
       };
     };
   };
