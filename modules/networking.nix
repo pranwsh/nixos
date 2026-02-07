b2ef78c999ef79e07cdfb96b2aa8d07969ad2b25
pranwsh <praneshk119@gmail.com>
2026-02-07 13:02:06 -0800

    services.resolved.fallbackDns deprecated

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,6 +1,9 @@
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   networking.useNetworkd = true;
   systemd.network.enable = true;
   networking.networkmanager.enable = false;
@@ -19,11 +22,21 @@
 
   services.resolved = {
     enable = true;
-    fallbackDns = [ "1.1.1.1" "8.8.8.8" ];
+    settings.Resolve.FallbackDns = ["1.1.1.1" "8.8.8.8"];
   };
 
   networking.firewall = {
-    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
-    allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
   };
 }
