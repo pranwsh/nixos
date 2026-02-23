3e03e6a7381418cf51cd6a7781cd721ca16baf40
pranwsh <praneshk119@gmail.com>
2026-02-23 13:32:09 -0800

    automatic display switching handling

--- home/hyprland/monitors.nix
+++ home/hyprland/monitors.nix
@@ -1,7 +1,34 @@
 {...}: {
-  wayland.windowManager.hyprland.settings = {
-    monitor = [
-      "eDP-1, 1920x1080@60, 0x0, 1"
-    ];
+  services.kanshi = {
+    enable = true;
+    systemdTarget = "graphical-session.target";
+    profiles = {
+      undocked = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            mode = "1920x1080@60.164001";
+            position = "0,0";
+            scale = 1.0;
+          }
+        ];
+      };
+      docked = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+            position = "-1920,0";
+          }
+          {
+            criteria = "HDMI-A-1";
+            mode = "2560x1080@60.000000";
+            position = "0,0";
+            scale = 1.0;
+          }
+        ];
+      };
+    };
   };
 }
