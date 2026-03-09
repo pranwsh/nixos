32335f43c044e989c8a50013f589a2e796cdc87e
pranwsh <praneshk119@gmail.com>
2026-03-08 19:15:37 -0700

    usb perms

--- hosts/nixos.nix
+++ hosts/nixos.nix
@@ -19,6 +19,7 @@
     ../modules/boot.nix
     ../modules/kvm.nix
     ../modules/fish.nix
+    ../modules/usb.nix
     ../users/users.nix
   ];
 
---
+++ modules/usb.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # Add user to plugdev group for USB device access
+  users.users.pranesh.extraGroups = ["plugdev" "dialout"];
+
+  # Optional: Enable udev support for EV3 devices
+  # This ensures proper permissions for LEGO EV3 USB devices
+  services.udev.extraRules = ''
+    # LEGO EV3 USB access
+    ATTR{idVendor}=="0694", ATTR{idProduct}=="0003", MODE="0666", GROUP="plugdev"
+    # Alternative EV3 vendor ID
+    ATTR{idVendor}=="0694", MODE="0666", GROUP="plugdev"
+  '';
+
+  # Ensure plugdev group exists
+  users.groups.plugdev = {};
+}
