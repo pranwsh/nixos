bc0904f560980f28c46a786f7996731b12e4bd9e
pranwsh <praneshk119@gmail.com>
2026-04-28 18:26:19 -0700

    refactor(dev/latex): simplify texlive package and remove .keep file entry

--- home/dev/code/latex/default.nix
+++ home/dev/code/latex/default.nix
@@ -2,12 +2,7 @@
 
 {
   home.packages = with pkgs; [
-    (texlive.combine {
-      inherit (texlive) scheme-full;
-    })
+    texlive.combined.scheme-full
     texlab
-    zathura
   ];
-
-  home.file."code/latex/.keep".text = "";
 }
