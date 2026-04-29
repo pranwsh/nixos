6b6e2e9bb8a6084838c8afebd3ad62dadfc004f2
pranwsh <praneshk119@gmail.com>
2026-04-28 18:25:46 -0700

    refactor(dev/go): update packages and remove .keep file entry

--- home/dev/code/go/default.nix
+++ home/dev/code/go/default.nix
@@ -4,12 +4,8 @@
   home.packages = with pkgs; [
     go
     gopls
-    delve
-    gotests
-    gomodifytags
-    impl
     golangci-lint
+    go-tools
+    delve
   ];
-
-  home.file."code/go/.keep".text = "";
 }
