bb1fa388d8d0f6a08a4dde8fc1234f65ae2bbbe0
pranwsh <praneshk119@gmail.com>
2026-04-28 18:24:50 -0700

    refactor(home): remove packages moved to separate modules

--- home/default.nix
+++ home/default.nix
@@ -10,19 +10,6 @@
   home.username = osConfig.my.user.name;
   home.homeDirectory = "/home/${osConfig.my.user.name}";
 
-  home.packages = [
-    pkgs.wineWow64Packages.stable
-    pkgs.discord
-    pkgs.libreoffice
-    pkgs.qemu
-    pkgs.chromium
-    pkgs.bluetui
-    pkgs.impala
-    pkgs.yazi
-    pkgs.gemini-cli
-    pkgs.qwen-code
-  ];
-
   home.stateVersion = "24.11";
   programs.home-manager.enable = true;
 }
