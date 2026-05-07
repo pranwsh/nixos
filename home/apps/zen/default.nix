4cc2100208eedaf3684a7dada733b5fb2bb801be
pranwsh <praneshk119@gmail.com>
2026-05-07 07:51:37 -0700

    feat(apps/zen): switch to home-manager module and configure profiles

--- home/apps/zen/default.nix
+++ home/apps/zen/default.nix
@@ -4,28 +4,41 @@
   pkgs,
   inputs,
   ...
-}: let
+}:
+let
   profileName = osConfig.my.user.zenProfileName;
   theme = config.style;
-in {
-  home.packages = [
-    inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
+in
+{
+  imports = [
+    inputs.zen-browser.homeModules.default
   ];
+
+  programs.zen-browser = {
+    enable = true;
+    profiles.${profileName} = {
+      isDefault = true;
+      search = {
+        force = true;
+        default = "ddg";
+      };
+    };
+  };
 
   home.file.".zen/${profileName}/chrome" = {
     source = ./config/chrome;
     recursive = true;
   };
-
   home.file.".zen/${profileName}/user.js" = {
-    text = let 
-      original = builtins.readFile ./config/user.js;
-    in pkgs.lib.replaceStrings 
-      ["/home/pranesh" "pranesh’s" "praneshk119@gmail.com"] 
-      ["${config.home.homeDirectory}" "${osConfig.my.user.fullName}’s" "${osConfig.my.user.email}"] 
-      original;
+    text =
+      let
+        original = builtins.readFile ./config/user.js;
+      in
+      pkgs.lib.replaceStrings
+        [ "/home/pranesh" "pranesh's" "praneshk119@gmail.com" ]
+        [ "${config.home.homeDirectory}" "${osConfig.my.user.fullName}'s" "${osConfig.my.user.email}" ]
+        original;
   };
-
   home.file.".zen/${profileName}/chrome/nix-colors.css" = {
     text = ''
       :root {
