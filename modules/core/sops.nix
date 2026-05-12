f125369cdbdbb72e46d93f5f212f3de43e94981d
pranwsh <praneshk119@gmail.com>
2026-05-12 00:52:11 -0700

    feat(sops): configure sops-nix secrets

---
+++ modules/core/sops.nix
@@ -0,0 +1,12 @@
+{ config, ... }: {
+  sops = {
+    defaultSopsFile = ../../secrets.yaml;
+    defaultSopsFormat = "yaml";
+    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+
+    secrets = {
+      deepseek_key = { };
+      nvidia_key = { };
+    };
+  };
+}
