7b3c9f1fa645cd5aa0150d8e6dc53a7c874214be
pranwsh <praneshk119@gmail.com>
2026-05-12 02:06:03 -0700

    fix: explicitly set user home to /home/pranesh with mkForce

--- users/users.nix
+++ users/users.nix
@@ -7,6 +7,7 @@
   users.users."${config.my.user.name}" = {
     isNormalUser = pkgs.lib.mkForce true;
     isSystemUser = pkgs.lib.mkForce false;
+    home = pkgs.lib.mkForce "/home/${config.my.user.name}";
     extraGroups = [
       "wheel"
       "video"
