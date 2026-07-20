aa8e15bed41ca195e629496cf7a142ebefb606d6
pranwsh <praneshk119@gmail.com>
2026-07-19 23:28:06 -0700

    python: reformat withPackages for readability

--- home/dev/code/python/default.nix
+++ home/dev/code/python/default.nix
@@ -2,7 +2,15 @@
 
 {
   home.packages = with pkgs; [
-    (python3.withPackages (ps: with ps; [ ddgs beautifulsoup4 requests aiohttp lxml ]))
+    (python3.withPackages (
+      ps: with ps; [
+        ddgs
+        beautifulsoup4
+        requests
+        aiohttp
+        lxml
+      ]
+    ))
     (pipx.overridePythonAttrs (oldAttrs: {
       doCheck = false;
     }))
