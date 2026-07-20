3d6838990c01e05e5f390c944acadd1b2193f539
pranwsh <praneshk119@gmail.com>
2026-07-20 16:24:51 -0700

    update kitty

--- home/dev/kitty.nix
+++ home/dev/kitty.nix
@@ -2,11 +2,9 @@
   config,
   pkgs,
   lib,
-  walNix,
   ...
 }: let
   theme = config.style;
-  c = (import "${walNix}/colors.nix").colorscheme;
 in {
   home.packages = with pkgs; [
     kitty
@@ -17,36 +15,36 @@ in {
 
     settings = {
       enable_audio_bell = "no";
-      background_opacity = lib.mkForce (toString theme.opacity.bg);
+      background_opacity = lib.mkForce (toString theme.opacity);
       confirm_os_window_close = "0";
       cursor_trail = "1";
     };
 
     extraConfig = ''
-      foreground ${c.foreground}
-      background #000000
-      cursor     ${c.cursor}
+      foreground ${theme.foreground}
+      background ${theme.background}
+      cursor     ${theme.colors.color15}
 
-      selection_foreground ${c.foreground}
-      selection_background ${c.color8}
-      url_color ${c.color6}
+      selection_foreground ${theme.foreground}
+      selection_background ${theme.colors.color8}
+      url_color ${theme.colors.color6}
 
-      color0  ${c.color0}
-      color1  ${c.color1}
-      color2  ${c.color2}
-      color3  ${c.color3}
-      color4  ${c.color4}
-      color5  ${c.color5}
-      color6  ${c.color6}
-      color7  ${c.color7}
-      color8  ${c.color8}
-      color9  ${c.color9}
-      color10 ${c.color10}
-      color11 ${c.color11}
-      color12 ${c.color12}
-      color13 ${c.color13}
-      color14 ${c.color14}
-      color15 ${c.color15}
+      color0  ${theme.colors.color0}
+      color1  ${theme.colors.color1}
+      color2  ${theme.colors.color2}
+      color3  ${theme.colors.color3}
+      color4  ${theme.colors.color4}
+      color5  ${theme.colors.color5}
+      color6  ${theme.colors.color6}
+      color7  ${theme.colors.color7}
+      color8  ${theme.colors.color8}
+      color9  ${theme.colors.color9}
+      color10 ${theme.colors.color10}
+      color11 ${theme.colors.color11}
+      color12 ${theme.colors.color12}
+      color13 ${theme.colors.color13}
+      color14 ${theme.colors.color14}
+      color15 ${theme.colors.color15}
     '';
   };
 }
