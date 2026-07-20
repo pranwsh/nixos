07150e16af87c3b6aab537945bac01a618073cb4
pranwsh <praneshk119@gmail.com>
2026-07-19 23:28:06 -0700

    nvim: remove commented lazy treesitter setup

--- home/dev/nvim/nvim/lua/init.lua
+++ home/dev/nvim/nvim/lua/init.lua
@@ -69,8 +69,3 @@ require("core.lsp").setup(lsp_servers)
 require("core.lint").setup(lint_by_ft)
 require("core.treesitter").setup()
 require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
-
--- vim.api.nvim_create_autocmd("User", {
---   pattern  = "VeryLazy",
---   callback = function() require("core.treesitter").setup() end,
--- })
