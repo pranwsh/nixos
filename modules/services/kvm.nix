30c0080de484d7bb525b9b55b2681e7bd9da66ce
pranwsh <praneshk119@gmail.com>
2026-07-28 02:30:08 -0700

    services: remove docker.nix and kvm.nix

--- modules/services/kvm.nix
+++
@@ -1,5 +0,0 @@
-{pkgs, config, ...}: {
-  virtualisation.libvirtd.enable = true;
-  users.users."${config.my.user.name}".extraGroups = ["libvirtd" "kvm"];
-  virtualisation.libvirtd.qemu.package = pkgs.qemu_kvm;
-}
