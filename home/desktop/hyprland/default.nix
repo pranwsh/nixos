8c67475f5a39c1e50ee23f14819197fa68670e19
pranwsh <praneshk119@gmail.com>
2026-08-04 16:36:10 -0700

    hyprland: switch config to lua

--- home/desktop/hyprland/default.nix
+++ home/desktop/hyprland/default.nix
@@ -15,7 +15,7 @@
 
   wayland.windowManager.hyprland = {
     enable = true;
-    configType = "hyprlang";
+    configType = "lua";
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
   };
 }
