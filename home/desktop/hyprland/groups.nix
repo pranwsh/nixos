13db825b9101b48194db8f9721c6ddc3fa217410
pranwsh <praneshk119@gmail.com>
2026-08-04 16:36:19 -0700

    hyprland: rewrite group settings in lua

--- home/desktop/hyprland/groups.nix
+++ home/desktop/hyprland/groups.nix
@@ -3,21 +3,29 @@
 let
   theme = config.style;
   stripHash = s: lib.removePrefix "#" s;
-  argb = a: hex: "0x${a}${stripHash hex}";
+  argb = a: hex: "rgba(${stripHash hex}${a})";
 in
 {
   wayland.windowManager.hyprland.settings = {
-    "group:groupbar:rounding" = 3;
-    "group:groupbar:rounding_power" = 2.0;
-    "group:groupbar:round_only_edges" = false;
-    "group:groupbar:height" = 6;
-    "group:groupbar:indicator_height" = 6;
-    "group:groupbar:font_size" = 0;
-    "group:groupbar:render_titles" = false;
-    "group:groupbar:text_color" = "0x00000000";
+    config = {
+      group = {
+        groupbar = {
+          rounding = 3;
+          rounding_power = 2.0;
+          round_only_edges = false;
+          height = 6;
+          indicator_height = 6;
+          font_size = 2;
+          render_titles = false;
+          text_color = 0;
+          blur = true;
 
-    "group:groupbar:blur" = true;
-    "group:groupbar:col.active" = argb "ff" theme.colors.color12;
-    "group:groupbar:col.inactive" = argb "80" theme.colors.color4;
+          col = {
+            active = argb "ff" theme.colors.color12;
+            inactive = argb "80" theme.colors.color4;
+          };
+        };
+      };
+    };
   };
 }
