9257c4d8154c1483131ff988340268b2c9393718
pranwsh <praneshk119@gmail.com>
2026-08-04 16:36:14 -0700

    hyprland: rewrite hyprshot binds in lua

--- home/desktop/hyprland/hyprshot.nix
+++ home/desktop/hyprland/hyprshot.nix
@@ -1,5 +1,8 @@
-{pkgs, ...}: {
-
+{
+  pkgs,
+  lib,
+  ...
+}: {
   home.packages = with pkgs; [
     hyprshot
   ];
@@ -8,11 +11,26 @@
 
   wayland.windowManager.hyprland.settings = {
     bind = [
-      "SUPER, Z, exec, ${pkgs.hyprshot}/bin/hyprshot -m output -o ~/Pictures/Screenshots"
-      
-      "SUPER SHIFT, Z, exec, ${pkgs.hyprshot}/bin/hyprshot -m window -o ~/Pictures/Screenshots"
-      
-      "SUPER CTRL, Z, exec, ${pkgs.hyprshot}/bin/hyprshot -m region -o ~/Pictures/Screenshots"
+      {
+        _args = [
+          (lib.generators.mkLuaInline ''mainMod .. " + Z"'')
+          (lib.generators.mkLuaInline ''hl.dsp.exec_cmd("${pkgs.hyprshot}/bin/hyprshot -m output -o ~/Pictures/Screenshots")'')
+        ];
+      }
+
+      {
+        _args = [
+          (lib.generators.mkLuaInline ''mainMod .. " + SHIFT + Z"'')
+          (lib.generators.mkLuaInline ''hl.dsp.exec_cmd("${pkgs.hyprshot}/bin/hyprshot -m window -o ~/Pictures/Screenshots")'')
+        ];
+      }
+
+      {
+        _args = [
+          (lib.generators.mkLuaInline ''mainMod .. " + CTRL + Z"'')
+          (lib.generators.mkLuaInline ''hl.dsp.exec_cmd("${pkgs.hyprshot}/bin/hyprshot -m region -o ~/Pictures/Screenshots")'')
+        ];
+      }
     ];
   };
 }
