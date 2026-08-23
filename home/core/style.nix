360579c3491cf3673dcfb8f1ac151e47bccb370b
pranwsh <praneshk119@gmail.com>
2026-08-23 16:40:58 -0700

    wall

--- home/core/style.nix
+++ home/core/style.nix
@@ -41,7 +41,7 @@ in
   };
 
   config.style = {
-    wallpaperPath = ../wallpapers/grass;
+    wallpaperPath = ../wallpapers/vader;
 
     background = "#000000";
     backgroundRgb = hexToRgb config.style.background;
