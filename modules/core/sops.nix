51589aaa4b267e68ceefb3a36be05a614e3793f4
pranwsh <praneshk119@gmail.com>
2026-09-03 23:44:13 -0700

    sops: rotate age key, drop deepseek_key, add tokenrouter_key

--- modules/core/sops.nix
+++ modules/core/sops.nix
@@ -11,13 +11,17 @@
     age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
     secrets = {
-      deepseek_key = { };
       nvidia_key = {
         owner = config.my.user.name;
         group = "users";
         mode = "0440";
       };
       mistral_key = {
+        owner = config.my.user.name;
+        group = "users";
+        mode = "0440";
+      };
+      tokenrouter_key = {
         owner = config.my.user.name;
         group = "users";
         mode = "0440";
