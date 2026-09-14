6827010e8629d701afe200519e96460e68e2581d
pranwsh <praneshk119@gmail.com>
2026-09-14 04:57:44 -0700

    docker: replace rootless systemd service with fish function

--- home/dev/docker.nix
+++ home/dev/docker.nix
@@ -13,20 +13,10 @@
     DOCKER_HOST = "unix://$XDG_RUNTIME_DIR/docker.sock";
   };
 
-  systemd.user.services.docker = {
-    Unit = {
-      Description = "Docker Application Container Engine (Rootless)";
-      After = [ "network-online.target" ];
-      Wants = [ "network-online.target" ];
-    };
-    Service = {
-      Environment = "DOCKER_HOST=unix://%t/docker.sock";
-      ExecStart = "${pkgs.docker}/bin/dockerd-rootless";
-      Restart = "always";
-      RestartSec = 10;
-      Type = "simple";
-      LimitNOFILE = "infinity";
-    };
-    Install.WantedBy = [ "default.target" ];
+  programs.fish.functions.dockerd = {
+    description = "Start rootless Docker daemon (foreground, Ctrl-C to stop)";
+    body = ''
+      ${pkgs.docker}/bin/dockerd-rootless
+    '';
   };
 }
